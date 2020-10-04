250c0c614bac722df67f8c1cb0fc12c90ca8e8e1
Mattia De Rosa <mattiadr96@gmail.com>
2020-10-04 14:22:32 +0200

    disabled horriblesubs

--- MALstreaming.user.js
+++ MALstreaming.user.js
@@ -33,12 +33,11 @@
 // array of all streaming services
 const streamingServices = [
 	// anime
-	{ id: "nineanime",    type: "anime", name: "9anime",       domain: "https://9anime.to/"                },
-	{ id: "animetwist",   type: "anime", name: "Anime Twist",  domain: "https://twist.moe/"                },
-	{ id: "horriblesubs", type: "anime", name: "HorribleSubs", domain: "https://horriblesubs.info/"        },
+	{ id: "nineanime",  type: "anime", name: "9anime",      domain: "https://9anime.to/"                },
+	{ id: "animetwist", type: "anime", name: "Anime Twist", domain: "https://twist.moe/"                },
 	// manga
-	{ id: "mangadex",     type: "manga", name: "MangaDex",     domain: "https://mangadex.org/"             },
-	{ id: "mangaplus",    type: "manga", name: "MANGA Plus",   domain: "https://mangaplus.shueisha.co.jp/" },
+	{ id: "mangadex",   type: "manga", name: "MangaDex",    domain: "https://mangadex.org/"             },
+	{ id: "mangaplus",  type: "manga", name: "MANGA Plus",  domain: "https://mangaplus.shueisha.co.jp/" },
 ];
 // contains variable properties for anime/manga modes
 let properties = {};
@@ -407,122 +406,6 @@ searchSite["animetwist"] = function(id, title) {
 			} else {
 				// error
 				errorResults(id, "Anime Twist: " + resp.status);
-			}
-		}
-	});
-}
-
-/* horriblesubs */
-/*******************************************************************************************************************************************************************/
-const horriblesubs = {};
-horriblesubs.base = "https://horriblesubs.info/";
-horriblesubs.anime = horriblesubs.base + "shows/"
-horriblesubs.api = horriblesubs.base + "api.php?method=getshows&type=show&showid="
-horriblesubs.nextid = "&nextid=";
-
-horriblesubs.regexID = /(?<=hs_showid = )\d+/;
-horriblesubs.resultsPerPage = 12;
-horriblesubs.loadPage = 2;
-
-function parseEpisodes(jqPage, episodes) {
-	jqPage.each(function() {
-		let ep = parseInt(this.id);
-		let div = $(this).find(".rls-link").last();
-		let res = div.attr("id").split("-")[1];
-		let href = div.find(".hs-magnet-link > a").attr("href");
-		episodes[ep - 1] = {
-			text: `Ep ${ep} (${res})`,
-			href: href,
-		}
-	});
-}
-
-getEpisodes["horriblesubs"] = function(dataStream, url) {
-	// request id
-	GM_xmlhttpRequest({
-		method: "GET",
-		url: horriblesubs.anime + url,
-		onload: function(resp) {
-			if (resp.status == 200) {
-				// OK
-				let showid = resp.responseText.match(horriblesubs.regexID);
-
-				// request first page of results
-				GM_xmlhttpRequest({
-					method: "GET",
-					url: horriblesubs.api + showid,
-					onload: function(resp) {
-						if (resp.status == 200) {
-							// OK
-							let jqPage = $(resp.responseText);
-							let episodes = [];
-							// parse first page of episodes
-							parseEpisodes(jqPage, episodes);
-							// put episodes, may be overridden by next requests
-							putEpisodes(dataStream, episodes, undefined);
-							// check if you need to download another page
-							let latestEp = parseInt(jqPage.first().attr("id"));
-							let nextEp = parseInt(dataStream.parents(".list-item").find(properties.findProgress).find(".link").text()) + 1;
-							if (isNaN(nextEp)) nextEp = 0;
-
-							let reqPage = Math.floor((latestEp - nextEp) / horriblesubs.resultsPerPage);
-
-							// request n pages (avoids multiple requests to page 0)
-							for (let i = 0; i < horriblesubs.loadPage && reqPage > 0; i++) {
-								GM_xmlhttpRequest({
-									method: "GET",
-									url: horriblesubs.api + showid + horriblesubs.nextid + reqPage,
-									onload: function(resp) {
-										if (resp.status == 200) {
-											// OK
-											parseEpisodes($(resp.responseText), episodes);
-											// put episodes
-											putEpisodes(dataStream, episodes, undefined);
-										}
-									}
-								});
-								// next page
-								reqPage--;
-							}
-						}
-					}
-				});
-			} else {
-				// error
-				errorEpisodes(dataStream, "HorribleSubs: " + resp.status);
-			}
-		}
-	});
-}
-
-getEplistUrl["horriblesubs"] = function(partialUrl) {
-	return horriblesubs.anime + partialUrl;
-}
-
-searchSite["horriblesubs"] = function(id, title) {
-	GM_xmlhttpRequest({
-		method: "GET",
-		url: horriblesubs.anime,
-		onload: function(resp) {
-			if (resp.status == 200) {
-				// OK
-				let jqPage = $(resp.response);
-				let list = [];
-
-				let shows = jqPage.find(".ind-show > a");
-				shows.each(function() {
-					list.push({
-						title: this.text,
-						href:  this.pathname.split("/")[2],
-					});
-				});
-				// filter results
-				let results = list.filter(item => matchResult(item, title));
-				// callback
-				putResults(id, results);
-			} else {
-				// error
-				errorResults(id, "HorribleSubs: " + resp.status);
 			}
 		}
 	});
